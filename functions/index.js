a5cf07f45d118cb67783333e0ea6c931a264d0f1
ddawidroch <david.roch.2001@gmail.com>
2022-02-04 18:28:12 +0100

    bot working

---
+++ functions/index.js
@@ -0,0 +1,87 @@
+require("dotenv").config();
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const dbRef = admin.firestore().doc("tokens/demo");
+const twitterApi = require("twitter-api-v2").default;
+const twitterClient = new twitterApi({
+  clientId: process.env.TWITTER_CLIENT_ID,
+  clientSecret: process.env.TWITTER_SECRET,
+});
+
+const callbackURL =
+  "http://127.0.0.1:5000/idk-bot-636c6/us-central1/authCallback";
+
+// OpenAI API init
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+  organization: process.env.OPENAI_ORG
+});
+const openai = new OpenAIApi(configuration);
+
+// STEP 1 - Auth URL
+exports.auth = functions.https.onRequest(async(request, response) => {
+  const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
+    callbackURL,
+    { scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access'] }
+  );
+
+  // store verifier
+  await dbRef.set({ codeVerifier, state });
+
+  response.redirect(url);
+});
+
+// STEP 2 - Verify callback code, store access_token 
+exports.authCallback = functions.https.onRequest(async(request, response) => {
+  const { state, code } = request.query;
+
+  const dbSnapshot = await dbRef.get();
+  const { codeVerifier, state: storedState } = dbSnapshot.data();
+
+  if (state !== storedState) {
+    return response.status(400).send('Stored tokens do not match!');
+  }
+
+  const {
+    client: loggedClient,
+    accessToken,
+    refreshToken,
+  } = await twitterClient.loginWithOAuth2({
+    code,
+    codeVerifier,
+    redirectUri: callbackURL,
+  });
+
+  await dbRef.set({ accessToken, refreshToken });
+
+  const { data } = await loggedClient.v2.me(); // start using the client if you want
+
+  response.send(data);
+});
+
+// STEP 3 - Refresh tokens and post tweets
+exports.tweet = functions.https.onRequest(async(request, response) => {
+  const { refreshToken } = (await dbRef.get()).data();
+
+  const {
+    client: refreshedClient,
+    accessToken,
+    refreshToken: newRefreshToken,
+  } = await twitterClient.refreshOAuth2Token(refreshToken);
+
+  await dbRef.set({ accessToken, refreshToken: newRefreshToken });
+
+  const nextTweet = await openai.createCompletion('text-davinci-001', {
+    prompt: 'tweet something cool',
+    max_tokens: 64,
+  });
+
+  const { data } = await refreshedClient.v2.tweet(
+    nextTweet.data.choices[0].text
+  );
+
+  response.send(data);
+});
